7f6167d9ed64146e6c12b2f5a19d464896dac056
hardzc123 <zhangchihust@gmail.com>
2025-11-01 01:10:34 +0100

    feat WIP: apple watch

---
+++ ios-native/DeskEmojiRobot/DeskEmojiRobot/ContentView.swift
@@ -0,0 +1,24 @@
+//
+//  ContentView.swift
+//  DeskEmojiRobot
+//
+//  Created by Chi on 31.10.2025.
+//
+
+import SwiftUI
+
+struct ContentView: View {
+    var body: some View {
+        VStack {
+            Image(systemName: "globe")
+                .imageScale(.large)
+                .foregroundStyle(.tint)
+            Text("Hello, world!")
+        }
+        .padding()
+    }
+}
+
+#Preview {
+    ContentView()
+}
---
+++ ios-native/DeskEmojiRobot/DeskEmojiRobot/DeskEmojiRobotApp.swift
@@ -0,0 +1,17 @@
+//
+//  DeskEmojiRobotApp.swift
+//  DeskEmojiRobot
+//
+//  Created by Chi on 31.10.2025.
+//
+
+import SwiftUI
+
+@main
+struct DeskEmojiRobotApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
